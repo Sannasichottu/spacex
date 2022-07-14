eecb877b3b2efd80fb1d3ac40ae64ebb3e661e6b
sannasichottu <sannasichottu@gmail.com>
2022-07-14 20:50:02 +0530

    rocket page style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import './App.css';
 import'bootstrap/dist/css/bootstrap.min.css';
 import Rocket from './components/Routes/Rocket';
-//import Navbar from './components/Routes/Navbar';
+import Navbar from './components/Routes/Navbar';
 //import Home from './components/Routes/Home';
 function App() {
   return (
     <>
-    
+    <Navbar/>
     <Rocket/>
       
       
